e0d5c7854f327ec44c433805d93239365eaf2ce5
Siddhant356 <siddhantshukla356@gmail.com>
2020-05-29 10:34:37 +0530

    Jason parsing in flutter app

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:jsontwik/parsing_json/json_parsing.dart';
+import 'package:jsontwik/parsing_json/parsing_json_map.dart';
+
+void main(){
+  return runApp(MaterialApp(
+    home: JsonParsingMap(),
+  ));
+}
